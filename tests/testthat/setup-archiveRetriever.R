bc343512de6f03238fff8999abf5a537c32bae41
Lukas Isermann <luk.i@web.de>
2021-09-28 12:30:56 +0200

    Changes to tests

--- tests/testthat/setup-archiveRetriever.R
+++ tests/testthat/setup-archiveRetriever.R
@@ -1,5 +1,6 @@
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
-  dir = vcr::vcr_test_path("fixtures")
+  dir = vcr::vcr_test_path("fixtures"),
+  preserve_exact_body_bytes = TRUE
 ))
 vcr::check_cassette_names()
